321c90c95417684fe1624d8acbf22754dd55723b
KauZeR <45661857+KauZeR@users.noreply.github.com>
2020-04-08 09:46:08 +0200

    Update CalculsTest.java

--- src/test/java/CalculsTest.java
+++ src/test/java/CalculsTest.java
@@ -1,24 +1,14 @@
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
- 
-
 /**
  * 
  */
-
- 
-
 /**
  * @author flori
  *
  */
 class CalculsTest {
-
- 
-
     private Calculs c = null;
     
     /**
@@ -28,41 +18,27 @@ class CalculsTest {
     void setUp() throws Exception 
     {
         c = new Calculs(2 , 1);
-    }
-
- 
-
-    
+    }   
     /**
      * Test method for {@link Calculs#Calculs(int, int)}.
      */
     @Test
     void testCalculs() {
-
- 
-
         fail("Not yet implemented");
     }
-
  
-
     /**
      * Test method for {@link Calculs#multiplier()}.
      */
     @Test
     void testMultiplier() 
     {
-    
-        
         
         if (c.multiplier() != 2)
         {
-            fail("La methode multiplier ne fonctionne pas, 2*1 n'est pas égal à 2");
+            fail("La methode multiplier ne fonctionne pas, 2*1 n'est pas Ã©gal Ã  2");
         }
     }
-
- 
-
     /**
      * Test method for {@link Calculs#additionner()}.
      */
@@ -70,11 +46,8 @@ class CalculsTest {
     void testAdditionner() {
     	if (c.additionner()!= 3)
         {
-            fail("La methode additionner ne fonctionne pas, 2+1 n'est pas égal à 3");
+            fail("La methode additionner ne fonctionne pas, 2+1 n'est pas Ã©gal Ã  3");
         }    }
-
- 
-
     /**
      * Test method for {@link Calculs#diviser()}.
      */
@@ -82,12 +55,9 @@ class CalculsTest {
     void testDiviser() {
     	if (c.diviser() != 2)
         {
-            fail("La methode diviser ne fonctionne pas, 2/1 n'est pas égal à 2");
+            fail("La methode diviser ne fonctionne pas, 2/1 n'est pas Ã©gal Ã  2");
         }
     }
-
- 
-
     /**
      * Test method for {@link Calculs#soustraire()}.
      */
@@ -95,9 +65,7 @@ class CalculsTest {
     void testSoustraire() {
     	if (c.soustraire() != -1)
         {
-            fail("La methode soustraire ne fonctionne pas, 2-1 n'est pas égal à 1");
-        }    }
-
- 
-
+            fail("La methode soustraire ne fonctionne pas, 2-1 n'est pas Ã©gal Ã  1");
+        }    
+    }
 }
